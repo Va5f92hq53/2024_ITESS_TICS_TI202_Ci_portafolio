e276488d0076da0c99f47dc2a128b3c65f822608
Evelyn <evelynirisvigil@gmail.com>
2024-05-24 13:32:26 -0600

    Inicio del proyecto

---
+++ src/itess_tics_ti202_portafolio/Itess_tics_ti202_portafolio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package itess_tics_ti202_portafolio;
+
+/**
+ *
+ * @author Gateway
+ */
+public class Itess_tics_ti202_portafolio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
